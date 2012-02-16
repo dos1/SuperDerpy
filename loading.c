8147fa04739de8afa23d7dd0aef2c632dd04defc
Sebastian Krzyszkowiak <dos@dosowisko.net>
2012-02-16 17:56:16 +0100

    make font scalable

--- loading.c
+++ loading.c
@@ -32,7 +32,7 @@ void Loading_Load(struct Game *game) {
 	game->loading.loading_bitmap = al_create_bitmap(al_get_display_width(game->display), al_get_display_height(game->display));
 	al_set_target_bitmap(game->loading.loading_bitmap);
 	al_draw_scaled_bitmap(game->loading.image,0, 0, al_get_bitmap_width(game->loading.image), al_get_bitmap_height(game->loading.image), 0, 0, al_get_display_width(game->display), al_get_display_height(game->display),0);
-	al_draw_text(game->font, al_map_rgb(255,255,255), 30, al_get_display_height(game->display)-120, ALLEGRO_ALIGN_LEFT, "Loading...");
+	al_draw_text(game->font, al_map_rgb(255,255,255), al_get_display_width(game->display)*0.0234, al_get_display_height(game->display)*0.85, ALLEGRO_ALIGN_LEFT, "Loading...");
 	al_set_target_bitmap(al_get_backbuffer(game->display));
 	al_destroy_bitmap(game->loading.image);
 }
--- main.c
+++ main.c
@@ -92,7 +92,7 @@ int main(int argc, char **argv){
    }
    al_set_window_title(game.display, "Super Derpy: Muffin Attack");
    al_hide_mouse_cursor(game.display);
-   game.font = al_load_ttf_font("ShadowsIntoLight.ttf",72,0 );
+   game.font = al_load_ttf_font("ShadowsIntoLight.ttf",al_get_display_height(game.display)*0.09,0 );
    
    game.event_queue = al_create_event_queue();
    if(!game.event_queue) {
