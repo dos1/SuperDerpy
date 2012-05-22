3df8d2e2aff8e725e4d0d421a194174cde87d5da
Sebastian Krzyszkowiak <dos@dosowisko.net>
2012-05-22 21:30:34 +0200

    update docs

--- doc/html/search/variables_70.js
+++ doc/html/search/variables_70.js
@@ -2,7 +2,9 @@ var searchData=
 [
   ['page',['page',['../structIntro.html#aaea1b2d24c0c9a5437c59d727c79ebd1',1,'Intro']]],
   ['pause',['pause',['../structGame.html#a3fe420d2d9b47cbdbb26d4f68bb5a0f0',1,'Game']]],
-  ['pie',['pie',['../structLevel.html#a8910f05e860fca85c96ae45633cc244d',1,'Level::pie()'],['../structMenu.html#a397eb035f88ef8138c4ba29f3fc1d63c',1,'Menu::pie()']]],
+  ['pie',['pie',['../structMenu.html#a397eb035f88ef8138c4ba29f3fc1d63c',1,'Menu']]],
+  ['pie1',['pie1',['../structLevel.html#acf777bae2b7533d8eda0d9766ea402df',1,'Level']]],
+  ['pie2',['pie2',['../structLevel.html#a6b817f8a028cbb01ea9d7e1e6dbcdede',1,'Level']]],
   ['pie_5fbitmap',['pie_bitmap',['../structMenu.html#ab46ce3041264bac0aabde3abd7549877',1,'Menu']]],
   ['pig',['pig',['../structLevel.html#aacde4816ff59c4d9ab6f403758630b8f',1,'Level']]],
   ['pinkcloud',['pinkcloud',['../structMenu.html#aec110fca9c47ccf9aaf6e4e7589b86f2',1,'Menu']]],
